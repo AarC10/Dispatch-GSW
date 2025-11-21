edc234c82f09eabb31eb9bc64ee02f38c5ff493f
Aaron Chan <aarchan108@gmail.com>
2025-11-21 10:36:46 -0500

    Starter files

---
+++ src-tauri/gen/android/app/src/main/java/com/wild_west_rocketry/dispatch_gsw/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.wild_west_rocketry.dispatch_gsw
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
